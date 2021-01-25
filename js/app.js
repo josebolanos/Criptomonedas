cdeead878539bcc3606b3cfc3c895371a76276a7
jose <josebolanoslanzarote@gmail.com>
2021-01-25 14:54:08 +0000

    Creamos la primera parte entrando en la API y creando un select con algunuas criptomonedas y por ultimo creando un mensaje de error

---
+++ js/app.js
@@ -0,0 +1,85 @@
+const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+
+const objBusqueda = {
+    moneda:'',
+ 
+}
+  
+
+   
+
+
+// Crear un Promise
+const obtnerCriptomonedas = criptomonedas => new Promise( resolve => {
+    resolve(criptomonedas);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    consultarCriptomonedas();
+
+    formulario.addEventListener('submit', submitFormulario);
+
+    criptomonedasSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
+
+});
+
+function consultarCriptomonedas() {
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+    fetch(url)
+        .then ( respuesta => respuesta.json() )
+        .then ( resultado => obtnerCriptomonedas(resultado.Data) )
+        .then( criptomonedas => selectCriptomonedas(criptomonedas))
+}
+
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach( cripto => {
+        const { FullName, Name } = cripto.CoinInfo;
+
+        const option = document.createElement('option');
+        option.value = Name;
+        option.textContent = FullName;
+        criptomonedasSelect.appendChild(option);
+
+    })
+}function leerValor(e) {
+    objBusqueda[e.target.name] = e.target.value;
+    
+}
+
+
+
+function submitFormulario(e) {
+    e.preventDefault();
+
+    // Validar
+    const { moneda, criptomoneda} = objBusqueda;
+
+    if(moneda === '' || criptomoneda === '' ) {
+        mostrarAlerta(' Ambos campos son oblitorios')
+        return;
+    }
+}
+
+function mostrarAlerta(msg) {
+    // Para que no se repita la parte error
+    const existeError = document.querySelector('.error');
+
+    if(!existeError) {
+        const divMensaje = document.createElement('div');
+    divMensaje.classList.add('error');
+
+    // Mensaje de error
+    divMensaje.textContent = msg;
+
+    formulario.appendChild(divMensaje);
+
+    setTimeout(() => {
+        divMensaje.remove();
+    },3000)
+    }
+    
+}
